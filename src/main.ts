3fc013fdb7faf661b703a18f7b5ab921d94722e7
pavithrameddaduwage <pavithrameddaduwage@gmail.com>
2026-04-06 05:23:37 -0400

    ++changes++

--- src/main.ts
+++ src/main.ts
@@ -7,14 +7,14 @@ config();
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
 
-  await app.listen(3000);
+  await app.listen(4016);
   const endDate = new Date();
   endDate.setHours(23, 59, 59, 999);
   const startDate = new Date();
   startDate.setDate(startDate.getDate() - 14);
   startDate.setHours(0, 0, 0, 0);
 
-  console.log('Application is running on port 3000');
+  console.log('Application is running on port 4016');
   console.log(`Fetching sales data from ${startDate.toISOString()} to ${endDate.toISOString()}`);
 
 }
