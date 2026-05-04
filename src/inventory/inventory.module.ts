a9deaabb18692c765a4910d02851d7f744e85956
pavithrameddaduwage <pavithrameddaduwage@gmail.com>
2026-05-04 09:51:33 +0530

    ++changes++

--- src/inventory/inventory.module.ts
+++ src/inventory/inventory.module.ts
@@ -4,13 +4,14 @@ import { HttpModule } from '@nestjs/axios';
 import { AuthModule } from '../auth/auth.module';
 import { CommonModule } from '../common/common.module';
 import { AmazonInventoryByAsin } from './entities/amazon_inventory_by_asin.entity';
+import { ReportStatusEntity } from '../common/entities/report-status.entity';
 import { InventoryService } from './inventory.service';
 import { InventoryController } from './inventory.controller';
 import { InventoryScheduler } from './inventory.scheduler';
 
 @Module({
   imports: [
-    TypeOrmModule.forFeature([AmazonInventoryByAsin]),
+    TypeOrmModule.forFeature([AmazonInventoryByAsin, ReportStatusEntity]),
     HttpModule,
     AuthModule,
     CommonModule,
--- src/sales/sales.module.ts
+++ src/sales/sales.module.ts
@@ -5,13 +5,14 @@ import { AuthModule } from '../auth/auth.module';
 import { CommonModule } from '../common/common.module';
 import { AmazonSalesAggregate } from './entities/amazon_sales_aggregate.entity';
 import { AmazonSalesByAsin } from './entities/amazon_sales_by_asin.entity';
+import { ReportStatusEntity } from '../common/entities/report-status.entity';
 import { SalesService } from './sales.service';
 import { SalesController } from './sales.controller';
 import { SalesScheduler } from './sales.scheduler';
 
 @Module({
   imports: [
-    TypeOrmModule.forFeature([AmazonSalesAggregate, AmazonSalesByAsin]),
+    TypeOrmModule.forFeature([AmazonSalesAggregate, AmazonSalesByAsin, ReportStatusEntity]),
     HttpModule,
     AuthModule,
     CommonModule,
